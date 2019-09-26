b62865d6b5848efa8eec6bae80288a0b1b1e38dc
Luan Castheloge <luanccp@gmail.com>
2019-09-26 15:10:36 -0300

    [CORE] Reorganize the files

--- challenge.py
+++ challenge.py
@@ -7,61 +7,62 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 %matplotlib inline
-import utils
+from utils import *
+from plots import *
 
+def load_config():
+    ## Setando estilo dos graficos
+    plt.style.use(["bmh", "dark_background"])
 
-dataset = pd.read_csv('Dados/dataset.csv')
-
-# dataset.describe()
-# dataset.info()
-# dataset.ix[0:0].info()
-
-
-
-# df_uf = dataset[['UF']].drop_duplicates()
-# print(df_uf['UF'][0])
-
-# df_uf_PB = dataset[dataset.UF == 'PB']
-# df_uf_PB.describe()
-
-
-
-#%% PLOTANDO GRAFICO
-
-def plot_pizza_gender_equal(df):
-    data = pd.DataFrame(df['UF'].value_counts())
-    plt.rcParams['figure.figsize'] = (11,7)
-    
-    plt.bar(np.arange(len(data)), df['UF'].value_counts(), align='center')
-    plt.xticks(np.arange(len(data)), data['UF'].keys())
-    plt.ylabel('Municípios')
-    plt.title('Estados igualitários')
-
-    plt.show()
-
-
-
-
-
-
-# CHAMADA DAS FUNCOES
-
-# plot_pizza_gender(dataset,2514305)
-# plot_pizza_avg_UF(dataset,"PB")
-
-## Reunimos as cidades mais igualitárias
-citiesGenderEquals = utils.show_cities_gender_equals(dataset)
-
-## Pré processamos os dados dela
-datasetProcessed = utils.replace_nan(citiesGenderEquals)
-
-## Plotamos os estados mais igualitarios
-plot_pizza_gender_equal(datasetProcessed)
-
+#%%
 def main():
-    ## Ver relação Urbano X Rural
-    plot_pizza_rural_urbano(datasetProcessed)
 
+    ## Leitura dos dados
+    dataset = pd.read_csv('Dados/dataset.csv')
+
+    load_config()
+
+    ## Reunimos as cidades mais igualitárias
+    citiesGenderEquals = show_cities_gender_equals(dataset)
+
+    ## Pré processamos os dados dela
+    datasetProcessed = replace_nan(citiesGenderEquals)
+
+    ## Plotamos os estados mais igualitarios
+    plot_pizza_gender_equal(datasetProcessed)
+
+    ## Ver relação Urbano X Rural
+    # plot_pizza_rural_urbano(datasetProcessed)
+
+    ## Ver relação proprietarios dos domicilios GERAL
+    # plot_bar_2columns(datasetProcessed,
+    # "QTD_DOMICILIOS_RESP_HOMEM",
+    # "QTD_DOMICILIOS_RESP_MULHER",
+    # "Homem", 
+    # "Mulher", 
+    # "Pessoas",
+    # "Proprietários de domicílio")
+
+    ## Ver relação proprietarios dos domicilios MEDIA
+    # plot_pizza_qtd_domicilio(datasetProcessed)
+
+    ## Ver relação de casal com filhos ou sem filhos em cidades igualitárias
+    # plot_bar_2columns(datasetProcessed,
+    # "PERC_DOMICILIOS_NUCLEAR_CASAL_COM_FILHOS",
+    # "PERC_DOMICILIOS_NUCLEAR_CASAL_SEM_FILHOS",
+    # "Com filhos", 
+    # "Sem filhos", 
+    # "Casais com filhos",
+    # "Relação de casais com filhos")
+
+    ## Ver relação de renda entre homem e mulher
+    plot_bar_2columns(datasetProcessed,
+    "MEDIA_REND_NOMINAL_HOMENS",
+    "MEDIA_REND_NOMINAL_MULHERES",
+    "Homem", 
+    "Mulher", 
+    "Renda mensal (R$)",
+    "Relação de renda por genero")
 
 # # citiesGenderEquals = citiesGenderEquals.apply(replace_nan, axis=1)
 # print(citiesGenderEquals)
@@ -70,12 +71,4 @@ def main():
 # plot_by_uf(dataset, "PB")
 
 #%%
-
-## Mapear diferenca entre rural e urbano
-def plot_pizza_rural_urbano(df):
-    fig1, ax1 = plt.subplots()
-    ax1.pie([df['PERC_POPULACAO_URBANA'].mean(), df['PERC_POPULACAO_RURAL'].mean()], labels=["URBANO", "RURAL"], autopct='%1.1f%%', shadow=True, startangle=90)
-    ax1.axis('equal')
-    plt.show()
-
 main()
---
+++ plots.py
@@ -0,0 +1,107 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def plot_by_uf(df,uf):
+    sns.set(style="whitegrid")
+
+    # Initialize the matplotlib figure
+    f, ax = plt.subplots(figsize=(6, 55))
+
+    # genders = sns.load_dataset(dataset).sort_values("total", ascending=False)
+
+    sns.set_color_codes("muted")
+    sns.barplot(x="PERC_POPULACAO_FEMININA", y="NOME_CIDADE", data=df[df.UF == uf],label="FEM", color="b")
+
+    sns.set_color_codes("pastel")
+    sns.barplot(x="PERC_POPULACAO_MASCULINA", y="NOME_CIDADE", data=df[df.UF == uf],label="MAS", color="b")
+
+    # Add a legend and informative axis label
+    ax.legend(ncol=2, loc="lower right", frameon=True)
+    ax.set(xlim=(0, 100), ylabel="",xlabel="Automobile collisions per billion miles")
+    sns.despine(left=True, bottom=True)
+
+
+def plot_pizza_gender(df, city_id):
+    labels = 'MAS','FEM'
+    data = df[df.ID_CIDADE == city_id]
+    newdata = [data.PERC_POPULACAO_FEMININA[0], data.PERC_POPULACAO_MASCULINA[0]]
+    
+    plot_pizza(newdata, labels, data['NOME_CIDADE'][0])
+
+
+def plot_pizza(data, labels,title):
+    fig1, ax1 = plt.subplots()
+
+    ax1.pie(data, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
+    ax1.axis('equal')
+
+    ax1.legend(labels, title=title, loc=4)
+    plt.show()
+
+def plot_pizza_avg_UF(df, uf):
+    new_df = df[df.UF == uf]
+    data =[new_df['PERC_POPULACAO_FEMININA'].mean(), new_df['PERC_POPULACAO_MASCULINA'].mean()]
+
+    plot_pizza(data,['MAS','FEM'], uf)
+
+def plot_pizza_gender_equal(df):
+    data = pd.DataFrame(df['UF'].value_counts())
+    plt.rcParams['figure.figsize'] = (11,7)
+    
+    plt.bar(np.arange(len(data)), df['UF'].value_counts(), align='center')
+    plt.xticks(np.arange(len(data)), data['UF'].keys())
+    plt.ylabel('Municípios')
+    plt.title('Estados igualitários')
+
+    plt.show()
+
+
+## Mapear diferenca entre rural e urbano
+def plot_pizza_rural_urbano(df):
+    fig1, ax1 = plt.subplots()
+    ax1.pie([df['PERC_POPULACAO_URBANA'].mean(), df['PERC_POPULACAO_RURAL'].mean()], labels=["URBANO", "RURAL"], autopct='%1.1f%%', shadow=True, startangle=90)
+    ax1.axis('equal')
+
+    fig1.suptitle('Relação URB x RUR', fontsize=16)
+    plt.show()
+
+
+def plot_pizza_qtd_domicilio(df):
+    fig1, ax1 = plt.subplots()
+
+    ax1.pie([df['QTD_DOMICILIOS_RESP_HOMEM'].mean(), df['QTD_DOMICILIOS_RESP_MULHER'].mean()], labels=["Homem", "Mulher"], autopct='%1.1f%%', shadow=True, startangle=90)
+    ax1.axis('equal')
+    fig1.suptitle('Responsáveis pelo domicílio', fontsize=16)
+    plt.show()
+
+def plot_bar_2columns(df,var_1, var_2, label_1, label_2, label_y, title):
+    estados = df['UF'].unique()
+    labels = estados
+    men_means = []
+    women_means = []
+
+    for i in range(len(estados)):
+        teste = df[df.UF == estados[i]]
+        men_means.append(teste[var_1].mean())
+        women_means.append(teste[var_2].mean())
+
+
+    x = np.arange(len(labels))  
+
+    width = 0.35  
+
+    fig, ax = plt.subplots(figsize=(18, 6))
+    ax.bar(x - width/2, men_means, width, label=label_1)
+    ax.bar(x + width/2, women_means, width, label=label_2)
+
+    ax.set_ylabel(label_y)
+    ax.set_title(title)
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+    ax.legend()
+
+    fig.tight_layout()
+    
+    plt.show()
